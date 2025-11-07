2a655f0fff260c9b5e411f7b3aa9a3fb01ea89ee
vitorioAug <vito.augustodia@gmail.com>
2025-11-07 20:36:27 -0300

    Add DeliveryListener

--- src/main/java/sp/senac/br/food/order/DeliveryListener.java
+++ src/main/java/sp/senac/br/food/order/DeliveryListener.java
@@ -3,19 +3,20 @@ package sp.senac.br.food.order;
 import com.vaadin.flow.data.provider.ArrayUpdater.Update;
 import sp.senac.br.food.views.HomeView;
 
-public class KitchenListener implements OrderStatusObserver {
+
+public class DeliveryListener implements OrderStatusObserver {
 
     private final HomeView homeView;
 
-    public KitchenListener(HomeView homeView) {
+    public DeliveryListener(HomeView homeView) {
         this.homeView = homeView;
     }
 
     @Override
-    public void update(Order order) {
-        if (order.getStatus() == OrderStatus.PREPARING) {
-            homeView.addToKitchen(order);
-            homeView.addLog("Pedido #" + order.getId() + " enviado para a cozinha.");
+    public  void update(Order order) {
+        if (order.getStatus() == OrderStatus.IN_TRANSIT) {
+            homeView.addToDeliveryPane(order);
+            homeView.addLog("Pedido #" + order.getId() + " saiu para entrega.");
         }
     }
 
@@ -23,4 +24,6 @@ public class KitchenListener implements OrderStatusObserver {
     public Update orderStatusUpdated(Order order) {
         throw new UnsupportedOperationException("Unimplemented method 'orderStatusUpdated'");
     }
+    
+
 }
